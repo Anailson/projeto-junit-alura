2537858d6cc23de8ed9f501e92c2411cdb5b96d8
anailson <arsantos315@gmail.com>
2023-02-24 11:13:24 -0300

    Organizando o código de teste

--- src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -5,6 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.alura.tdd.modelo.Desempenho;
@@ -22,34 +26,53 @@ Se o desempenho for "Ótimo", o reajuste será de 20% do salário.
  */
 public class ReajusteServiceTest {
 	
+	private ReajusteService service;
+	private Funcionario funcionario;
+	
+	@BeforeEach//antes de cada metodo é chamado o metodo
+	public void inicializar() {
+		this.service = new ReajusteService();
+		this.funcionario = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000"));
+		
+	}
+	
+	@AfterEach
+	public void finalizar() {
+		System.out.println("fim");
+	}
+	
+	@BeforeAll
+	public static void antesDeTodos() {
+		System.out.println("ANTES DE TODOS");
+	}
+	
+	@AfterAll
+	public static void depoisDeTodos() {
+		System.out.println("DEPOIS DE TODOS");
+	}
+	
+	
 	@Test
 	public void reajusteDeveriaSerDeTresPorCentroDesempenhoADesejar() {
-		ReajusteService service = new ReajusteService();
-		Funcionario funcionario = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000"));
-		
-		service.concederReajuste(funcionario, Desempenho.A_Desejar);
-		
+	
+		service.concederReajuste(funcionario, Desempenho.A_Desejar);		
 		assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
 		
 	}
 	
 	@Test
 	public void desempenhoBom() {
-		ReajusteService service = new ReajusteService();
-		Funcionario funcionario = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000"));
-		
-		service.concederReajuste(funcionario, Desempenho.A_Bom);
-		
+	
+	
+		service.concederReajuste(funcionario, Desempenho.A_Bom);		
 		assertEquals(new BigDecimal("1150.00"), funcionario.getSalario());
 		
 	}
 	@Test
 	public void desempenhoOtimo() {
-		ReajusteService service = new ReajusteService();
-		Funcionario funcionario = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000"));
-		
-		service.concederReajuste(funcionario, Desempenho.A_Otimo);
-		
+			
+	
+		service.concederReajuste(funcionario, Desempenho.A_Otimo);		
 		assertEquals(new BigDecimal("1200.00"), funcionario.getSalario());
 		
 	}
